9ad686facb02771d8e9e758614bb6072cc28960f
Dipak Patel <dap9107@gmail.com>
2021-05-04 21:55:49 -0400

    Format

--- register_provider.php
+++ register_provider.php
@@ -116,8 +116,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($stmt = mysqli_prepare($link, $sql)) {
                 // Bind variables to the prepared statement as parameters
                 mysqli_stmt_bind_param(
-                    $stmt,
-                    "ssiissss",
+                    $stmt,"ssiissss",
                     $param_providerName,
                     $param_providerAddress,
                     $param_providerLatitude,
