fae34fde19e7d48c53343fadb4f1d8729e60cd4a
Iffat Rahman <iffat.rahman92@gmail.com>
2021-05-05 20:50:05 -0400

    Add packages for dashboard code

--- header.php
+++ header.php
@@ -10,17 +10,43 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" crossorigin="anonymous">
 
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="vendor/parsley/parsley.css"/>
+
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap-select/bootstrap-select.min.css"/>
+
+    <!-- <link rel="stylesheet" type="text/css" href="vendor/datepicker/bootstrap-datepicker.css"/> -->
+
 </head>
 
 <body>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
-	<nav class="navbar navbar-dark bg-dark" style="margin-bottom: 40px;">
+	<nav class="navbar navbar-dark" style="background-color:#3c4b64!important;">
 		<div class="container-fluid">
 			<i class="fas fa-hand-holding-medical fa-3x" style="color: white; margin-right: 5px;"></i>
 			<a class="navbar-brand" style="font:28px sans-serif">COVID VACCINE PORTAL</a>
 			<?php
+			// Initialize the session
+				session_start();
 			if (!isset($_SESSION['loggedin'])) {
 			?>
 				<ul class="navbar-nav ms-auto flex" style="flex-direction: row;">
@@ -33,6 +59,14 @@
 				</ul>
 
 			<?php
+			} else{
+				?>
+								<ul class="navbar-nav ms-auto flex" style="flex-direction: row;">
+					<li class="nav-item">
+						<a href="logout.php" class="nav-link m-2 menu-item nav-active">Log Out</a>
+					</li>
+				</ul>
+				<?php
 			}
 			?>
 			</form>
