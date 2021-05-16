1ae839f319411f9b42cf124bdbbc159447d3d628
Iffat Rahman <iffat.rahman92@gmail.com>
2021-05-15 23:45:00 -0400

    Remove unused plugins from header.php

--- header.php
+++ header.php
@@ -9,29 +9,25 @@
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- fonts for this template-->
-	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>        
+	 	
 	<!-- styles for this template-->
-	<link href="css.min.css" rel="stylesheet">
-	<link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="vendor/parsley/parsley.css" />
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap-select/bootstrap-select.min.css" />
-	<link rel="stylesheet" type="text/css" href="vendor/datepicker/bootstrap-datepicker.css" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link href="css.min.css" rel="stylesheet">
+	<!-- Bootstrap Date-Picker Plugin -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" /> 
+	<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>  
+	<script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap.min.js"></script>   
 	
 
 	<style>
         body {
+			color: black;
             font: 14px sans-serif;
         }
 
